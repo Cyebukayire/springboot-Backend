d14751c996503a6c8663df52efaa9e1f6efc25cb
Cyebukayire <peacecyebukayire@gmail.com>
2021-07-31 17:04:44 +0200

    Backend working

--- src/main/java/net/javaguides/SpringbootBackendApplication.java
+++ src/main/java/net/javaguides/SpringbootBackendApplication.java
@@ -1,13 +1,32 @@
 package net.javaguides;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import net.javaguides.model.User;
+import net.javaguides.repository.UserRepository;
+
 @SpringBootApplication
-public class SpringbootBackendApplication {
+public class SpringbootBackendApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendApplication.class, args);
+	}
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		this.userRepository.save(new User("Peace", "Hanson", "peace@gmail.com"));  
+		this.userRepository.save(new User("Kellia", "Umuhire", "kellia@gmail.com"));
+		this.userRepository.save(new User("Gisa", "Fadhili", "gisa@gmail.com"));
+		this.userRepository.save(new User("Sam", "Shallom", "sam@gmail.com"));
+
+
 	}
 
 }
--- src/main/java/net/javaguides/controller/UserController.java
+++ src/main/java/net/javaguides/controller/UserController.java
@@ -1,5 +1,26 @@
 package net.javaguides.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.javaguides.model.User;
+import net.javaguides.repository.UserRepository;
+
+@RestController
+@RequestMapping("api/")
 public class UserController {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@GetMapping("users")
+	public List<User> getUsers(){
+		return this.userRepository.findAll();
+		
+	}
 
 }
